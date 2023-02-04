72de09f34023de77962102aa26063073fe2f924f
strda <jshull@mail.bradley.edu>
2023-02-04 13:34:56 -0600

    Second Iteration

---
+++ EscapeRoom/Source/EscapeRoom/MyFirstCharacter.cpp
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyFirstCharacter.h"
+
+// Sets default values
+AMyFirstCharacter::AMyFirstCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMyFirstCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMyFirstCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AMyFirstCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	PlayerInputComponent->BindAxis(TEXT("MoveForward"), this, & AMyFirstCharacter::MoveForward);
+	PlayerInputComponent->BindAxis(TEXT("MoveRight"), this, &AMyFirstCharacter::MoveRight);
+	PlayerInputComponent->BindAxis(TEXT("Turn"), this, &AMyFirstCharacter::AddControllerYawInput);
+	PlayerInputComponent->BindAxis(TEXT("LookUp"), this, &AMyFirstCharacter::AddControllerPitchInput);
+	PlayerInputComponent->BindAction(TEXT("Jump"), IE_Pressed, this, & AMyFirstCharacter::Jump);
+	PlayerInputComponent->BindAction(TEXT("Jump"), IE_Released, this, &AMyFirstCharacter::StopJumping);
+
+}
+
+void AMyFirstCharacter::MoveForward(float AxisVal)
+{
+	AddMovementInput(GetActorForwardVector() * AxisVal);
+}
+
+void AMyFirstCharacter::MoveRight(float AxisVal)
+{
+	AddMovementInput(GetActorRightVector() * AxisVal);
+}
+
